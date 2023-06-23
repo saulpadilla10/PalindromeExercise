0bc940e291d9a8624933c475c45c82b87fcfeab3
saulpadilla10 <saulp1021@yahoo.com>
2023-06-23 00:09:58 -0700

    Palidrome

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+
+		}
+		
+		public bool IsAPalindrome(string word)
+		{
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            string reversed = new string(charArray);
+
+            return word.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+        }
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Wow", true)]
+        [InlineData("Phillip", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            WordSmith challenger = new WordSmith();
 
+            bool actual = challenger.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
